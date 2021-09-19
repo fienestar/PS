6126441129dfa1bc8e257d7539a729db2031f582
fienestar <fienestar@gmail.com>
2021-09-20 05:26:12 +0900

    Solve boj_20543.cpp

--- code/boj_20543.cpp
+++ code/boj_20543.cpp
@@ -3,50 +3,45 @@
     https://www.acmicpc.net/problem/20543
 */
 #include <bits/stdc++.h>
+
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
-#define all(v) v.begin(), v.end()
-const ll MX = 101010;
-const ll INF = 9e15;
-const ll MOD = 998'244'353;
-
-#define endl '\n'
-
-int n, m;
-ll arr[2005][2005];
-ll ans[2005][2005];
+using ll = long long;
 
 int main()
 {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	
+	size_t n, m;
 	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			cin >> arr[i][j];
 
-	for (int j = m / 2; j < n - m / 2; ++j) {
+	size_t hm = m >> 1;
+
+	vector<vector<ll>> v(n, vector<ll>(n));
+	for(auto& line:v)
+		for(auto& dot:line)
+			cin >> dot;
+
+	vector<vector<ll>> ans(n, vector<ll>(n));
+	for(size_t j=hm; j+hm!=n; ++j){
 		ll sum = 0;
-		for (int i = m / 2; i < n - m / 2; ++i) {
-			ans[i][j] = -arr[i - m / 2][j - m / 2] - sum;
-			sum += ans[i][j];
-			if (i - (m - 1) > 0)
-				sum -= ans[i - (m - 1)][j];
+		for(size_t i=hm; i+hm!=n; ++i){
+			sum += ans[i][j] = -v[i-hm][j-hm] - sum;
+			if(i >= m)
+				sum -= ans[i-m+1][j];
 		}
 	}
 
-	for (int i = m / 2; i < n - m / 2; ++i) {
+	for(size_t i=hm; i+hm!=n; ++i){
 		ll sum = 0;
-		for (int j = m / 2; j < n - m / 2; ++j) {
-			ans[i][j] -= sum;
-			sum += ans[i][j];
-			if (j - (m - 1) > 0)
-				sum -= ans[i][j - (m - 1)];
+		for(size_t j=hm; j+hm!=n; ++j){
+			sum += ans[i][j] -= sum;
+			if(j >= m)
+				sum -= ans[i][j-m+1];
 		}
 	}
 
-	for (int i = 0; i < n; ++i,cout<<endl)
-		for (int j = 0; j < n; ++j)
-			cout << ans[i][j] << ' ';
+	for(auto& line:ans){
+		for(auto dot:line)
+			cout << dot << " ";
+		cout << "\n";
+	}
 }
