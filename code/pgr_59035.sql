a00323fb9d2417c5709bc2b3caf692d2104c5aeb
fienestar <fienestar@gmail.com>
2022-05-28 14:55:50 +0900

    Solve pgr_59035.sql

---
+++ code/pgr_59035.sql
@@ -0,0 +1,9 @@
+/*
+    Lv. 1
+                
+                역순 정렬하기
+    https://programmers.co.kr/learn/courses/30/lessons/59035
+*/
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC
